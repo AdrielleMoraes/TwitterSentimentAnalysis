aa433bd5bf3643d3fb42d0dec041e9727ee3c2cc
Adrielle <adrielle.nazar@hotmail.com>
2021-03-06 17:51:17 -0300

    prepro function ready

--- server/preProcessing.py
+++ server/preProcessing.py
@@ -1,14 +1,12 @@
-import pandas as pd
-from sklearn import preprocessing
-import contractions
+import numpy as np
 import nltk
-from nltk.tokenize import word_tokenize 
-import string
-from spellchecker import SpellChecker
-from nltk.corpus import wordnet
-import nltk
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from yellowbrick.text import PosTagVisualizer
+import contractions
+import re
 
 # function to convert nltk tag to wordnet tag
 def nltk_tag_to_wordnet_tag(nltk_tag):
@@ -29,59 +27,49 @@ def cleaning_email(data):
 def cleaning_URLs(data):
     return re.sub('((www\.[^\s]+)|(https?://[^\s]+))',' ',data)
 
+def text_prePro(text):
+    if isinstance(text, str):
+        
+        # Contract the text data
+        text = [contractions.fix(word) for word in text.split()]
+        text = ' '.join(map(str, text))
 
-#import tweets
-tweets_path = ""
-tweets = pd.read_table(tweets_path, header=None)
+        # Remove digits
+        text= re.sub(r'\d+', '', text)
 
-#Label encode data - only use this if data is not encoded yet
-le = preprocessing.LabelEncoder() # assaign tags to data as 0 for ham and 1 for spam 
-y = tweets[0] #first column contains the label for a tweet
-y_enc = le.fit_transform(y)
+        # lower case
+        text= str.lower(text)
 
-raw_text = tweets[1] # get text from database
+        # Remove user mentions ( in case of tweet replies )
+        text=cleaning_email(text)
 
-# check for missing values
-pd.isnull(tweets).count()
+        # Remove any URL
+        text = cleaning_URLs(text)
 
-tweets.columns = ["label", "msg"] # assign labels to columns
+        # Remove punctuation
+        text = re.sub(r'[^\w\d\s\']+', '', text)
 
-# Contract the text data
-tweets['msg'] = tweets['msg'].apply(lambda x: [contractions.fix(word) for word in x.split()]) # loop trough each word in db to remove contractions
-tweets['msg'] = [' '.join(map(str, l)) for l in tweets['msg']] #joining back the list of items into one string
+        # Remove duplicate white spaces
+        text = re.sub(' +', ' ',text)
 
-# Remove digits
-tweets['msg'] = tweets['msg'].apply(lambda x: re.sub(r'\d+', '', x))
+        # Tokenization 
+        text_token = word_tokenize(text)
 
-# lower case
-tweets['msg'] = tweets['msg'].apply(lambda x: str.lower(x))
-
-# Remove user mentions ( in case of tweet replies )
-tweets['msg']= tweets['msg'].apply(lambda x: cleaning_email(x))
-
-# Remove any URL
-tweets['msg'] = tweets['msg'].apply(lambda x: cleaning_URLs(x))
-
-# Remove punctuation
-tweets['msg'] = tweets['msg'].apply(lambda x: re.sub(r'[^\w\d\s\']+', '', x))
-
-# Remove duplicate white spaces
-tweets['msg'] = tweets['msg'].apply(lambda x: re.sub(' +', ' ',x))
-
-# Tokenization 
-tweets['tokenized'] = tweets['msg'].apply(word_tokenize)
-
-# Remove stop words
-stop_words = set(stopwords.words('english'))
-tweets['no_stop'] = tweets['tokenized'].apply(lambda x: [word for word in x if word not in stop_words])
+        # Remove stop words
+        stop_words = set(stopwords.words('english'))
+        text_stop = [word for word in text_token if word not in stop_words]
 
 
-# Tag words according to their classification
-# noun, verb, adjectives, adverb...
-tweets['pos_tag'] =  tweets['no_stop'].apply(nltk.tag.pos_tag)
+        # Tag words according to their classification
+        # noun, verb, adjectives, adverb...
+        text_pos = nltk.tag.pos_tag(text_stop)
 
-# Lemmatization
-#This step is more accurate if POS tag are given
-lemmatizer=WordNetLemmatizer()
-tweets['lemmatized']= tweets['pos_tag'].apply(lambda x: [lemmatizer.lemmatize(word[0], pos=nltk_tag_to_wordnet_tag(word[1])) for word in x])
-tweets['total'] = [' '.join(map(str, l)) for l in tweets['lemmatized']] #joining back the list of items into one string
+        # Lemmatization
+        #This step is more accurate if POS tag are given
+        lemmatizer=WordNetLemmatizer()
+        text_lemmatised = [lemmatizer.lemmatize(word[0], pos=nltk_tag_to_wordnet_tag(word[1])) for word in text_pos]
+        return ' '.join(map(str, text_lemmatised)) #joining back the list of items into one string
+
+    else:
+        print("Enter a valid string")
+
