92bbf5dbfe1209b64b2235416eda73d4f591c35b
Adrielle <adrielle.nazar@hotmail.com>
2021-03-25 14:18:20 -0300

    facial expression model implemented

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import twitterAPI as api
-
+from camera import VideoCamera
 
 app = Flask(__name__)
 
@@ -17,6 +17,21 @@ def retrieveFromAPI():
     #TODO sanitize query
     apiResponse = api.queryOnTwitterAPI(query=query, count=20)
     return apiResponse
+
+@app.route('/camera')
+def retrieveFromAPI():
+    return("Hello")
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
