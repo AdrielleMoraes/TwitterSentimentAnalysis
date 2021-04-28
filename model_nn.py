10e331a3d823734f1833a046aa74bb443e4b3bdd
Adrielle <adrielle.nazar@hotmail.com>
2021-04-28 16:16:06 +0100

    update weights

--- model_nn.py
+++ model_nn.py
@@ -1,7 +1,7 @@
-from model import FacialExpressionModel
+from model import TweetPolarityModel
 import numpy as np
 
-model = TweetPolarityModel("Models/model.json", "Models/model_weights.h5")
+model = TweetPolarityModel("Models/model.json", "Models/model_weights_v0.h5")
 
 def predictText(text):
     return model.predict_polarity(text)
