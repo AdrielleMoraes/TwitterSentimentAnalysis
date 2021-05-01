1232d8d32b69d50fe42c5b6563b2376a6299187f
Adrielle <adrielle.nazar@hotmail.com>
2021-05-01 10:46:07 +0100

    deploying to github pages

---
+++ src/Components/TabMenu/TabMenu.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import TweetsList from "../TweetsList/TweetsList"
+import Chart from "../Graphs/Chart"
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: `${100}%`,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function TabMenu(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab className={classes.tab} label="Tweets" {...a11yProps(0)} />
+        <Tab className={classes.tab} label="Distribution" {...a11yProps(1)} />
+      </Tabs>
+
+      <TabPanel value={value} index={0}>
+        {props.showAPIResponse ? <TweetsList APIresponse={props.data} /> : <p>Enter items on search bar</p>}
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        {props.showAPIResponse ? <Chart/>: <p>Enter items on search bar</p>}
+      </TabPanel>
+    </div>
+  );
+}
---
+++ src/Components/TweetsList/TweetsList.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { List } from 'semantic-ui-react'
+import Tweet from "./Tweet/Tweet"
+import "./TweetsList.css"
+
+const TweetsList = (props) => {
+
+    return (
+        <div className="TweetsList" >
+            <h1>Tweets</h1>
+            <p>Showing {props.APIresponse.tweets.length} results</p>
+            <p>Results for query: {props.APIresponse.query}</p>
+            <List selection divided verticalAlign='middle'>
+                {props.APIresponse.tweets.map(tweet => {
+                    return <Tweet
+                        name={tweet.author_name}
+                        key={tweet.id}
+                        id={tweet.id}
+                        created_at={tweet.created_at}
+                        text={tweet.text}
+                        text_pre = {tweet.pre}
+                        hashtags={tweet.hashtags}
+                        favourite_count={tweet.favourite_count}
+                    />
+                })}
+            </List>
+        </div>
+
+    )
+}
+
+export default TweetsList
